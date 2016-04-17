bd9154e2a111f3399176e64d5172f1b743d31032
Knut Sveidqvist <knut@sveido.com>
2016-04-17 08:11:13 +0200

    Added scheduler, some logging fixes in crib-mq

--- src/index.js
+++ src/index.js
@@ -2,21 +2,7 @@
  * Created by knut on 2016-04-16.
  */
 
-
-
-// io.on('connection', function (socket) {
-//     socket.emit('news', { hello: 'world' });
-//     socket.on('my other event', function (data) {
-//         console.log(data);
-//     });
-// });
-
-
-
 module.exports.create = function(port){
-// note, io(<port>) will create a http server for you
-
-
 
     console.log('Starting message queue');
     var io         = require('socket.io')();
@@ -26,17 +12,14 @@ module.exports.create = function(port){
 
 
     io.on('connection', function (socket) {
-        console.log('Got connection ...');
-
-        io.emit('this', { will: 'be received by everyone'});
 
         socket.on('*', function(message){
-            console.log('Got => ',message.data[0],' ',message.data[1],' passing it on');
+            console.log('Got => ',message.data[0],' ',message.data[1]);
             io.emit(message.data[0], message.data[1]);
 
         });
 
-        socket.on('disconnect', function () { console.log('Got disconnect');});
+        socket.on('disconnect', function (data) { console.log('Got disconnect',data);});
 
 
     });
@@ -48,8 +31,9 @@ module.exports.register = function(name , bussUrl){
     console.log('Registering client ',name);
     var io = require('socket.io-client');
     var socket = io.connect(bussUrl, {reconnect: true});
-    socket.on('this', function () { // TIP: you can avoid listening on `connect` and listen on events directly too!
-            console.log('got this'); // data will be 'woot'
+    socket.on('connect', function () { // TIP: you can avoid listening on `connect` and listen on events directly too!
+        console.log('Connected'); // data will be 'woot'
+        socket.emit('REGISTER_CLIENT',name);
     });
     return socket;
 };

e161d914ec31f76c95bf3e71f5ec64dc6f3abc22
Knut Sveidqvist <knut@sveido.com>
2016-08-27 16:45:18 +0200

    Latest version with logging

--- src/api.js
+++ src/api.js
@@ -2,6 +2,9 @@
  * Created by knut on 2016-04-16.
  */
 
+var cribLog = require('../../crib-log/src/api');
+var log = cribLog.createLogger('crib-mq-client','debug');
+
 module.exports.create = function(port){
 
     console.log('Creating message queue');
@@ -14,7 +17,7 @@ module.exports.create = function(port){
     io.on('connection', function (socket) {
 
         socket.on('*', function(message){
-            console.log('Got => ',message.data[0],' ',message.data[1]);
+            log.trace('Got => ',message.data[0],' ',message.data[1]);
             io.emit(message.data[0], message.data[1]);
 
         });
@@ -27,12 +30,17 @@ module.exports.create = function(port){
     io.listen(port);
 };
 
-module.exports.register = function(name , bussUrl){
-    console.log('Registering client ',name);
+module.exports.register = function(name,url){
+    let bussUrl = url;
+    if(!url){
+        bussUrl = process.env.CRIB_BUSS_URL;
+    }
+
+    log.info('Registering client ',name,' to url = ', bussUrl);
     var io = require('socket.io-client');
     var socket = io.connect(bussUrl, {reconnect: true});
     socket.on('connect', function () { // TIP: you can avoid listening on `connect` and listen on events directly too!
-        console.log('Connected'); // data will be 'woot'
+        log.debug('Connected'); // data will be 'woot'
         socket.emit('REGISTER_CLIENT',name);
     });
     return socket;
